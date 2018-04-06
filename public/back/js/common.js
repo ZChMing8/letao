3ab721320603be9b657dde48a9e2361c806a4d4f
ZChMing8 <1030373607@qq.com>
2018-04-06 23:59:14 +0800

    下班了

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -15,3 +15,27 @@ $(document).ajaxStop(function () {
       NProgress.done();
     },500)
 })
+
+$(function () {
+    $(".nav ul>li>a").each(function () {
+        $(this).on("click",function () {
+          $(this).addClass("current");
+          $(this).parent().siblings().children("a").removeClass("current");
+        })
+    })
+  $(".category").on("click",function () {
+    $(this).next().stop().slideToggle();
+  })
+  
+  //点击icon_menu 让侧边栏慢慢显示与隐藏
+  $(".icon_menu").on("click",function () {
+      
+  })
+  
+  //模态框，点击使其显示
+  $(".icon_logout").on("click",function () {
+    console.log(1);
+    $('#myModal').modal("show");
+  })
+})
+
