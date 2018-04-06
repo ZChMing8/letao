81ed2fbaea88a22b6f58ad1f9ff25e3e6aa877a9
ZChMing8 <1030373607@qq.com>
2018-04-06 21:37:56 +0800

    公共进度条部分完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,3 +1,17 @@
 /**
  * Created by 赵春明 on 2018/4/6.
  */
+
+
+// 配置禁用小圆环
+NProgress.configure({ showSpinner: false });
+
+$(document).ajaxStart(function () {
+    NProgress.start();
+})
+
+$(document).ajaxStop(function () {
+    setInterval(function () {
+      NProgress.done();
+    },500)
+})
